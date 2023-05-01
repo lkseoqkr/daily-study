0aca461339ac03e137864cc045ca72d2ac48dac1
Will-san <lkseoqkr@gmail.com>
2023-05-02 05:37:57 +0900

    2013-04-24

---
+++ programming-language/java/gs-consuming-rest/gs-gradle/src/main/java/jp/co/study/gsgradle/GsGradleApplication.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsGradleApplication.class, args);
+	}
+
+}
---
+++ programming-language/java/gs-consuming-rest/gs-gradle/src/test/java/jp/co/study/gsgradle/GsGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
