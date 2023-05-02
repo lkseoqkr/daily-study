8a14f16715fe78991d584e5ea2c898ab10253c71
Will-san <lkseoqkr@gmail.com>
2023-05-02 23:00:18 +0900

    2013-05-02 Accessing Relational Data using JDBC with Spring

---
+++ programming-language/java/gs-relational-data-access/src/test/java/jp/co/study/gsrelationaldataaccess/GsRelationalDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsrelationaldataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRelationalDataAccessApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
