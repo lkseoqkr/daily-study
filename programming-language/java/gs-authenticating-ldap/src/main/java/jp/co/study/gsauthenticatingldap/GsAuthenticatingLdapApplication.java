ab48623bad8dde8c835e693cad188c6f797fca08
Will-san <lkseoqkr@gmail.com>
2023-05-06 14:10:34 +0900

    2013-05-06 Authenticating a User with LDAP

---
+++ programming-language/java/gs-authenticating-ldap/src/main/java/jp/co/study/gsauthenticatingldap/GsAuthenticatingLdapApplication.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsauthenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAuthenticatingLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAuthenticatingLdapApplication.class, args);
+	}
+
+}
---
+++ programming-language/java/gs-authenticating-ldap/src/test/java/jp/co/study/gsauthenticatingldap/GsAuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsauthenticatingldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
