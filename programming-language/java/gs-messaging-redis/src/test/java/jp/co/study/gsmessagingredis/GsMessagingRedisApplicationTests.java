e4669790d748d0275e9157e1f8419ee92f044abb
Will-san <lkseoqkr@gmail.com>
2023-05-13 22:31:20 +0900

    Messaging with Redis

---
+++ programming-language/java/gs-messaging-redis/src/test/java/jp/co/study/gsmessagingredis/GsMessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsmessagingredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsMessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
