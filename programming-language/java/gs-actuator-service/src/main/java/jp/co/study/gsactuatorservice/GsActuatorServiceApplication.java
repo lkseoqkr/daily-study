aa34d0b08fb0f765f869bfdcfdb91c27d8fc3430
Will-san <lkseoqkr@gmail.com>
2023-05-18 08:55:23 +0900

    Building a RESTful Web Service with Spring Boot Actuator

---
+++ programming-language/java/gs-actuator-service/src/main/java/jp/co/study/gsactuatorservice/GsActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsactuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsActuatorServiceApplication.class, args);
+	}
+
+}
