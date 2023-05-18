cd491332f5df2fa51253a04d67ac8b469768779a
Will-san <lkseoqkr@gmail.com>
2023-05-19 08:59:19 +0900

    Messaging with JMS

---
+++ programming-language/java/gs-messaging-jms/src/test/java/jp/co/study/gsmessagingjms/GsMessagingJmsApplicationTests.java
@@ -0,0 +1,13 @@
+//package jp.co.study.gsmessagingjms;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GsMessagingJmsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
