092c0ae39df2dfa850b943b9bc712624946567ae
Will-san <lkseoqkr@gmail.com>
2023-05-20 12:14:22 +0900

    Creating a Batch Service

---
+++ programming-language/java/gs-batch-processing/src/main/java/jp/co/study/gsbatchprocessing/GsBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		System.exit(SpringApplication.exit(SpringApplication.run(GsBatchProcessingApplication.class, args)));
+	}
+
+}
---
+++ programming-language/java/gs-batch-processing/src/test/java/jp/co/study/gsbatchprocessing/GsBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsbatchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
