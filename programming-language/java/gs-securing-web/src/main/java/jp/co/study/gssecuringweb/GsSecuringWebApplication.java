eeefac58c09d55dc1c498ca4c7ffaffc6ddc427a
Will-san <lkseoqkr@gmail.com>
2023-05-21 21:15:39 +0900

    Securing a Web Application

---
+++ programming-language/java/gs-securing-web/src/main/java/jp/co/study/gssecuringweb/GsSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package jp.co.study.gssecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSecuringWebApplication.class, args);
+	}
+
+}
