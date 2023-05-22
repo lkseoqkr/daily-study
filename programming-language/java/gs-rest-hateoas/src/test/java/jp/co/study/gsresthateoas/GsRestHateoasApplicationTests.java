4cd17b0088bd7b9d4a8dfbd7c6ca5d1fd5d91962
Will-san <lkseoqkr@gmail.com>
2023-05-23 08:49:55 +0900

    Building a Hypermedia-Driven RESTful Web Service

---
+++ programming-language/java/gs-rest-hateoas/src/test/java/jp/co/study/gsresthateoas/GsRestHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsresthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRestHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
