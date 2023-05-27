397beac9aee7f41fccd88cf4eef0f499f99b0776
Will-san <lkseoqkr@gmail.com>
2023-05-27 21:49:13 +0900

    Accessing Data with JPA

---
+++ programming-language/java/gs-accessing-data-jpa/src/test/java/jp/co/study/gsaccessingdatajpa/GsAccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsaccessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
