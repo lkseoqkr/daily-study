93fadb695583c4e57861bb67b8a398947a71cc96
Will-san <lkseoqkr@gmail.com>
2023-05-28 13:59:09 +0900

    Accessing Data with MongoDB

---
+++ programming-language/java/gs-accessing-data-mongodb/src/main/java/jp/co/study/gsaccessingdatamongodb/CustomerRepository.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsaccessingdatamongodb;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+  public Customer findByFirstName(String firstName);
+  public List<Customer> findByLastName(String lastName);
+
+}
+
---
+++ programming-language/java/gs-accessing-data-mongodb/src/test/java/jp/co/study/gsaccessingdatamongodb/GsAccessingDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.study.gsaccessingdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
