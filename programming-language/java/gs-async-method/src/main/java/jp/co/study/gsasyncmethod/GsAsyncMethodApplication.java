4efa9f9dcf2a9daabfb3d1be58ea7ee0770cece8
Will-san <lkseoqkr@gmail.com>
2023-05-31 09:25:06 +0900

    Creating Asynchronous Methods

---
+++ programming-language/java/gs-async-method/src/main/java/jp/co/study/gsasyncmethod/GsAsyncMethodApplication.java
@@ -0,0 +1,30 @@
+package jp.co.study.gsasyncmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@SpringBootApplication
+public class GsAsyncMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAsyncMethodApplication.class, args);
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(3);
+		executor.setMaxPoolSize(3);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		return executor;
+	}
+
+}
